b583fbbac14225853d9a77a0a25344760ef8c3da
Vegardlarm <97906599+Vegardlarm@users.noreply.github.com>
2023-07-14 00:11:38 +0200

    update 3

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import '../Css/Footer.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
   return (
-    <div className='Container'>Footer</div>
-  )
+    <div className='Container'>
+      <div className='SocialIcons'>
+        <a href="https://twitter.com">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://instagram.com">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://facebook.com">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://youtube.com">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+      </div>
+      <div className='FooterText'>
+        <FontAwesomeIcon icon={faCircle} className='circle-icon' />
+        <span className='copyright'>
+          Gamer Goals, Inc. All rights reserved.
+        </span>
+      </div>
+    </div>
+  );
 }
+
