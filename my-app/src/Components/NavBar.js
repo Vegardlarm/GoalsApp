3c4b977ba996865f276786edc0d48fec6a8f30c5
Vegardlarm <97906599+Vegardlarm@users.noreply.github.com>
2023-07-19 16:47:27 +0200

    update

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
 import '../Css/NavBar.css';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-  const location = useLocation(); 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/" className="navbar-logo">Goals</Link>
-      </div>
-      <div className="navbar-right">
-        <ul className="navbar-links">
-          <li className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}>
-            <Link to="/" className="navbar-link-text">Home</Link>
-          </li>
-          <li className={`navbar-link ${location.pathname === '/about' ? 'active' : ''}`}>
-            <Link to="/about" className="navbar-link-text">About</Link>
-          </li>
-          <li className={`navbar-link ${location.pathname === '/tasks' ? 'active' : ''}`}>
-            <Link to="/tasks" className="navbar-link-text">Tasks</Link>
-          </li>
+      <div className="logo">Goals</div>
+      <div className={`menu-container ${isMenuOpen ? "open" : ""}`}>
+        <ul className="menu">
+          <li><Link to="/" className="navbar-link-text">Home</Link></li>
+          <li><Link to="/about" className="navbar-link-text">About</Link></li>
+          <li><Link to="/tasks" className="navbar-link-text">Tasks</Link></li>
+          <Link to="/login"><button className="sign-up-button">Sign up</button></Link>
         </ul>
-        <Link to="/login" className="navbar-button-link">
-          <button className="navbar-button">Sign up</button>
-        </Link>
+      </div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
       </div>
     </nav>
   );
--- my-app/src/Components/Tasks.js
+++ my-app/src/Components/Tasks.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import TaskList from './TaskList';
-import ThreeDComponent from './ThreeDComponent';
+
 
 const Tasks = () => {
   return (
     <div>
       <h1>Welcome to the task page</h1>
       <TaskList />
-      <ThreeDComponent />
+      
     </div>
   );
 };
